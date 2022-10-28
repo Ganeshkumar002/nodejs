1a20409ff93128c44b202f980bbbb14ecdb91ada
Ganeshkumar002 <kumarganesh4458@gmail.com>
2022-10-28 23:22:37 +0530

    assigmnt2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,51 @@
+
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+const bookSchema = new mongoose.Schema(
+    {
+        bookName:{ 
+            type:String,
+        
+        },
+        price:Number,
+        year:{ type:Number,default:2021},
+        tag:[String],
+        authorName:String,
+        totalPages:Number,
+        stockAvailable:Boolean
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    {   timestamps : true}
+);
+module.exports = mongoose.model('bookName',bookSchema)
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const bookSchema = new mongoose.Schema( {
+//     bookName: String, 
+//     authorName: String, 
+//     tags: [String],
+    
+//     isPublished: Boolean,
+//     prices: {
+//         indianPrice: String,
+//         europePrice: String,
+//     },
+//     sales: {type: Number, default: 10}
+// }, { timestamps: true });
+
+
+// module.exports = mongoose.model('Book', bookSchema) //users
 
 //Validation:
 //require:true
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const bookingData= require("../controllers/bookController.js")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.post("/createBook",bookingData.createBook);
 
-router.post("/createUser", UserController.createUser  )
+router.get("/bookList",bookingData.bookList);
 
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/getBooksYear",bookingData.getBookYear);
 
-router.post("/createBook", BookController.createBook  )
+router.post("/getParticularBooks",bookingData.getParticularBooks);
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/getXINRBooks",bookingData.getXINRBooks);
+
+router.get("/getRandomBooks",bookingData.getRandomBooks);
+
+//  const UserModel= require("../models/userModel.js")
+// const UserController= require("../controllers/userController")
+// const BookController= require("../controllers/bookController")
+
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
+
+// router.post("/createBook", BookController.createBook  )
+
+// router.get("/getBooksData", BookController.getBooksData)
 
 module.exports = router;
